68cc28c224d6b165d2b713e0332d8ca19ad4fe7e
YoheiMD555 <denndy9@gmail.com>
2024-06-10 01:46:30 +0900

    Update service-worker.js
    Githubがホストのコードに書き換え

--- service-worker.js
+++ service-worker.js
@@ -1,45 +1,21 @@
-const CACHE_NAME = 'PediMed-cache-v1';
-const urlsToCache = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  '/service-worker.js',
-  '/assets/icons/PediMed_logo_trimmed.png'
-  // 必要に応じて他のファイルを追加
-];
-
-self.addEventListener('install', event => {
+self.addEventListener('install', function(event) {
     event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(cache => {
-                return cache.addAll(urlsToCache);
-            })
+        caches.open('static-v1').then(function(cache) {
+            return cache.addAll([
+                '/',
+                '/index.html',
+                '/manifest.json',
+                '/assets/icons/PediMed_logo_trimmed.png'
+                // 必要に応じて他のファイルを追加
+            ]);
+        })
     );
 });
 
-self.addEventListener('fetch', event => {
+self.addEventListener('fetch', function(event) {
     event.respondWith(
-        caches.match(event.request)
-            .then(response => {
-                if (response) {
-                    return response; // キャッシュがヒットした場合
-                }
-                return fetch(event.request); // キャッシュがヒットしなかった場合
-            })
-    );
-});
-
-self.addEventListener('activate', event => {
-    const cacheWhitelist = [CACHE_NAME];
-    event.waitUntil(
-        caches.keys().then(cacheNames => {
-            return Promise.all(
-                cacheNames.map(cacheName => {
-                    if (cacheWhitelist.indexOf(cacheName) === -1) {
-                        return caches.delete(cacheName);
-                    }
-                })
-            );
+        caches.match(event.request).then(function(response) {
+            return response || fetch(event.request);
         })
     );
 });
