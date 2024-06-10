330e73e7f8926a9b374b693c305a74747cfd6dee
YoheiMD555 <denndy9@gmail.com>
2024-06-10 15:43:47 +0900

    Update calculate.js

--- calculate.js
+++ calculate.js
@@ -2,6 +2,7 @@
 import { calculateDosageForSFM2 } from './SFM2.js';
 import { calculateDosageForSFM3 } from './SFM3.js';
 import { calculateDosageForSFM4 } from './SFM4.js';
+
 import { calculateDosageForSFM6 } from './SFM6.js';
 import { calculateDosageForSFM7 } from './SFM7.js';
 import { calculateDosageForSFM8 } from './SFM8.js';
@@ -39,7 +40,7 @@ function calculateAgeDependentDosage(age) {
     }
 }
 
-function calculateDosage(diseaseType, weight, age, data) {
+export function calculateDosage(diseaseType, weight, age, data) {
     return data.map(row => {
         let dosage = getDosage(row, weight, age);
         let volume, description;
