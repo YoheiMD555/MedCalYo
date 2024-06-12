a849150b49f9741f55aff1795c5f1c7614abcbfd
YoheiMD555 <denndy9@gmail.com>
2024-06-12 18:28:32 +0900

    Update SFM10.js

--- SFM10.js
+++ SFM10.js
@@ -1,4 +1,6 @@
 // SFM10の計算関数
+import { roundToSigFigs } from './calculate.js';
+
 export function calculateDosageForSFM10(weight, row) {
     const dosagePerKg = parseFloat(row[7]);  // コラムHから用量を取得 (μg/kg)
     const maxDosage = parseFloat(row[9]);    // コラムJから最大用量を取得 (μg)
