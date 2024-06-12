b524f5890be2eeaffe0f5edd7cfd0613df09fa6c
YoheiMD555 <denndy9@gmail.com>
2024-06-12 17:19:49 +0900

    Update SFM3.js

--- SFM3.js
+++ SFM3.js
@@ -1,6 +1,6 @@
 export async function calculateDosageForSFM3(weight) {
     const spreadsheetId = '1rx2oDEms28Dpji01TiHoA5EXQpHnKq78ZF5BU6GktWk'; // SFM3のスプレッドシートID
-    const range = 'Sheet1!A1:L1000'; // 適切なシート名と範囲を指定してください
+    const range = 'Sheet1!A2:L1000'; // 適切なシート名と範囲を指定してください
     const apiKey = 'AIzaSyCu9ekb7iQWvmGi3TpOndM_ry7GjAFn9no'; // Google Sheets APIキーをここに入力
 
     const url = `https://sheets.googleapis.com/v4/spreadsheets/${spreadsheetId}/values/${range}?key=${apiKey}`;
