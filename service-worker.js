3aa0dcee8d1b59813b834a69554af38b2390e8ca
YoheiMD555 <denndy9@gmail.com>
2024-06-12 17:34:37 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,29 @@
-self.addEventListener('install', function(event) {
+self.addEventListener('install', event => {
     event.waitUntil(
-        caches.open('static-v1').then(function(cache) {
+        caches.open('static-v1').then(cache => {
             return cache.addAll([
-                '/',
-                '/index.html',
-                '/manifest.json',
-                '/assets/icons/PediMed_logo_trimmed.png'
-                // 必要に応じて他のファイルを追加
+                './',
+                './index.html',
+                './styles.css',
+                './calculate.js',
+                './SFM2.js',
+                './SFM3.js',
+                './SFM4.js',
+                './SFM6.js',
+                './SFM7.js',
+                './SFM8.js',
+                './SFM9.js',
+                './SFM10.js',
+                './assets/icons/icon-192x192.png',
+                './assets/icons/icon-512x512.png'
             ]);
         })
     );
 });
 
-self.addEventListener('fetch', function(event) {
+self.addEventListener('fetch', event => {
     event.respondWith(
-        caches.match(event.request).then(function(response) {
+        caches.match(event.request).then(response => {
             return response || fetch(event.request);
         })
     );
